b5a5b701732c30d6b100d9c704936d598681cf80
Carmelo Falsone <carmelo.falsone@studio.unibo.it>
2025-12-01 15:49:40 +0100

    Controller and SimpleGUI implemented

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,59 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private static final String DEFAULT = "output.txt";
+    private File file;
+
+    /**
+     * Create a single deafult file 
+     */
+    public Controller() {
+        this.file = new File(System.getProperty("user.home") + File.separator + DEFAULT);
+    }
+
+    /**
+     * Set the input file as the current file
+     * 
+     * @param f
+     */
+    public void setFile(File f) {
+        this.file = f;
+    }
+
+    /**
+     * Get the current file
+     * 
+     * @return
+     */
+    public File getFile() {
+        return this.file;
+    }
+
+    /**
+     * Get the string format of the path of the current file
+     * 
+     * @return
+     */
+    public String getPath() {
+        return this.file.toPath().toString();
+    }
+
+    /**
+     * Save in the current file the input string
+     * 
+     * @param input
+     * @throws IOException
+     */
+    public void saveInput(String input) throws IOException {
+        Files.writeString(file.toPath(), input);
+    }
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,60 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 20;
+    private final JFrame frame = new JFrame("Salva il testo");
+    private final Controller c;
+
+    public SimpleGUI() {
+        c = new Controller();
+        JPanel panel = new JPanel(new BorderLayout());
+        JTextArea text = new JTextArea();
+        JButton button = new JButton("Save");
+        panel.add(text, BorderLayout.NORTH);
+        panel.add(button, BorderLayout.SOUTH);
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    c.saveInput(text.getText());
+                } catch (IOException e1) {
+                    System.out.println("sono esploso"); // NOPMD
+                }
+            }
+        });
+    }
+
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 
 }
